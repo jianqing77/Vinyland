f78356cd6dc76d585f4ceaecf13ebb853324bd37
Jianiqng Ma <jianqing997@gmail.com>
2024-09-01 15:34:29 -0400

    created vinyl class and vinyl service, modified app.module for configuration

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { VinylListComponent } from './components/vinyl-list/vinyl-list.component';
+import { HttpClient } from '@angular/common/http';
+import { VinylService } from './services/vinyl.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    VinylListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration(),
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, VinylListComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClient],
+  providers: [provideClientHydration(), provideAnimationsAsync(), VinylService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ client/src/app/common/vinyl.spec.ts
@@ -0,0 +1,7 @@
+import { Vinyl } from './vinyl';
+
+describe('Vinyl', () => {
+  it('should create an instance', () => {
+    expect(new Vinyl()).toBeTruthy();
+  });
+});
