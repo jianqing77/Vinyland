9b6c776caabca9cdebea9f4faf9fa800452e21e7
Jianiqng Ma <jianqing997@gmail.com>
2024-09-01 15:50:37 -0400

    modified service to use httpClient to fetch data from backend API

--- client/src/app/services/vinyl.service.ts
+++ client/src/app/services/vinyl.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Vinyl } from '../common/vinyl';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class VinylService {
 
-  constructor() { }
+// using the HttpClient to fetch data from the backend API
+export class VinylService {
+  private baseUrl = 'http://localhost:8080/api/vinyls';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getVinylList(theCategoryId: number): Observable<Vinyl[]> {
+    return this.httpClient
+      .get<GetResponse>(this.baseUrl)
+      .pipe(map((response) => response._embedded.vinyls));
+  }
+}
+
+// supporter interface
+// unwraps the JSON from Spring Data REST _embedded entity
+interface GetResponse {
+  _embedded: {
+    vinyls: Vinyl[];
+  };
+  page: {
+    size: number;
+    totalElements: number;
+    totalPages: number;
+    number: number;
+  };
 }
